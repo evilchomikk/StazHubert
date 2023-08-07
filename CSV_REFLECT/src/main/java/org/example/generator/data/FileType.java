96a37bef33fd02d6069e7c7750e1e88376de5456
Hubert Kulka <119060681+evilchomikk@users.noreply.github.com>
2023-08-07 12:09:31 +0200

    Delete CSV_REFLECT directory

--- CSV_REFLECT/src/main/java/org/example/generator/data/FileType.java
+++
@@ -1,9 +0,0 @@
-package org.example.generator.data;
-
-public enum FileType {
-    CSV,
-    EXCEL,
-    YAML,
-    JSON,
-    XML
-}
--- CSV_REFLECT/src/main/java/org/example/generator/manager/interfaces/Generator.java
+++
@@ -1,9 +0,0 @@
-package org.example.generator.manager.interfaces;
-
-import org.example.generator.data.ClassFields;
-import org.example.generator.data.GeneratorData;
-
-public interface Generator {
-    void generate(String targetLoc, ClassFields classFields, GeneratorData generatorData)
-            throws Exception;
-}
